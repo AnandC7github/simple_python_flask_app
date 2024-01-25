00facad11c64b9f70a10d707fb47511da4c70022
Ajay-user <59962924+Ajay-user@users.noreply.github.com>
2024-01-25 08:08:28 +0000

    replit dev env

--- market/model.py
+++ market/model.py
@@ -1,5 +1,12 @@
 from market import db
 
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(length=30), nullable=False, unique = True)
+  email_address = db.Column(db.String(length=50), nullable=False, unique = True)
+  password_hashed = db.Column(db.String(length=60), nullable=False)
+  budget = db.Column(db.Integer(), nullable=False, default=1000)
+  items = db.relationship('Item', backref = 'owned_user',lazy=True)
 
 class Item(db.Model):
     """Data base table for ITEMS"""
@@ -8,6 +15,7 @@ class Item(db.Model):
     barcode = db.Column(db.String(), nullable=False)
     price = db.Column(db.Integer(), nullable=False)
     description = db.Column(db.String(), nullable=False)
-
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+  
     def __repr__(self) -> str:
         return f"ITEM < {self.name} | ${self.price} >"
