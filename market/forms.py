3383130e6e94d19545749a80fde734047b270407
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-02-13 05:56:18 +0000

    new coomit

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,22 @@
 '''Flask form for user registration'''
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators
-from wtforms.validators import Length, EqualTo, DataRequired
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import Length, EqualTo, Email, DataRequired
+from market.model import User
 
 
 class Form(FlaskForm):
+
+  def validate_username(self, username_to_check):
+    user = User.query.filter_by(username=username_to_check.data).first()
+    if user:
+      raise ValidationError('Username already exists! Please try a different name')
+
+    def vaildate_email(self, email_to_check):
+      email = User.query.filter_by(email = email_to_check.data).first()
+      if email:
+        raise ValidationError('Email already exists! Please try a different email')
+    
     '''User Registration | Sign-in and Sign-up'''
     username = StringField(label='Username', description="Username", validators=[Length(min=2,max=30),DataRequired()])
     email = StringField(label='Email', description="Email address", validators= [Email(), DataRequired()])
